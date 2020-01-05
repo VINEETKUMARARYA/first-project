3a7233b07df0b2316241f1ef5fdc7cc210857586
Vineet Kumar Arya <vineet.kumar.arya@gmail.com>
2020-01-05 20:17:39 +0530

    Angular Basics HandsOn_Data Binding, Displaying Data & Time, Configured Bootstrap 4

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DateTimeComponent } from './date-time/date-time.component';
+import { MainSectionComponent } from './main-section/main-section.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DateTimeComponent,
+    MainSectionComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
